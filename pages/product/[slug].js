565a66598b4052bc00d8d84df8f07f14a513e434
Abhinandan Maity <87464133+abhinandanmaity@users.noreply.github.com>
2024-07-29 14:42:50 +0530

    commit today

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -605,7 +605,7 @@ const Slug = ({ t, product, variants, similarproduct, seller, getseller }) => {
                                     {product.category == 'dairy' && <h1 className="text-gray-900 text-2xl title-font font-medium mb-1">{product.title} ({product.quantity} {product.mesure})</h1>}
 
                                     <div className="flex mb-4">
-                                        <span className="flex items-center">
+                                        {/* <span className="flex items-center">
                                             <svg fill="currentColor" stroke="currentColor" strokelinecap="round" strokelinejoin="round" strokewidth="2" className="w-4 h-4 text-pink-500" viewBox="0 0 24 24">
                                                 <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                                             </svg>
@@ -622,8 +622,8 @@ const Slug = ({ t, product, variants, similarproduct, seller, getseller }) => {
                                                 <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                                             </svg>
                                             <span className="text-gray-800 ml-3">4 Reviews</span>
-                                        </span>
-                                        <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
+                                        </span> */}
+                                        {/* <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
                                             <a className="text-gray-500">
                                                 <svg fill="currentColor" strokelinecap="round" strokelinejoin="round" strokewidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                                     <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
@@ -639,7 +639,7 @@ const Slug = ({ t, product, variants, similarproduct, seller, getseller }) => {
                                                     <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                                                 </svg>
                                             </a>
-                                        </span>
+                                        </span> */}
                                     </div>
 
                                     <div className="text-base sm:text-sm font-semibold pb-1">Product Details</div>
