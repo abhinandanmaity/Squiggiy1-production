6b23780f39889917e985426cd878f9d66af092d8
Abhinandan Maity <87464133+abhinandanmaity@users.noreply.github.com>
2024-09-19 23:13:29 +0530

    commit 19-09-2024

--- pages/user/mywishlist.js
+++ pages/user/mywishlist.js
@@ -19,7 +19,7 @@ const Mywishlist = ({ t, product }) => {
 
     // console.log(product)
     const router = useRouter();
-    const [product, setProduct] = useState(product);
+    const [products, setProducts] = useState(product);
 
     const handlesubmit = (p) => {
 
@@ -117,7 +117,7 @@ const Mywishlist = ({ t, product }) => {
                         progress: undefined,
                     });
 
-                    setProduct((prevProducts) =>
+                    setProducts((prevProducts) =>
                         prevProducts.filter(item => item.slug !== p.slug)
                     );
 
@@ -146,7 +146,7 @@ const Mywishlist = ({ t, product }) => {
 
     React.useEffect(() => {
 
-    }, [product])
+    }, [products])
     return (
 
         <div>
@@ -187,19 +187,19 @@ const Mywishlist = ({ t, product }) => {
                     <div className="-mt-16 sm:-mt-48 lg:-mt-32 xl:-mt-40 2xl:container 2xl:mx-auto flex justify-center items-center space-y-4 px-4 md:px-6 2xl:px-0 mb-16">
                         <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-between gap-x-6 gap-y-5">
 
-                            {product && Object.keys(product).map((item) => {
+                            {products && Object.keys(products).map((item) => {
 
                                 // console.log(similarproduct[item])
 
                                 return (
-                                    <div key={product[item].slug} className="flex flex-col justify-center items-start px-2 bg-white cursor-pointer">
+                                    <div key={products[item].slug} className="flex flex-col justify-center items-start px-2 bg-white cursor-pointer">
 
                                         <div className="relative">
 
 
-                                            <AiOutlineDelete onClick={() => { handledeletewishlist(product[item]) }} className="top-2 left-2 absolute  text-gray-900 hover:text-gray-500 flex justify-center items-center bg-white rounded-full text-base" />
+                                            <AiOutlineDelete onClick={() => { handledeletewishlist(products[item]) }} className="top-2 left-2 absolute  text-gray-900 hover:text-gray-500 flex justify-center items-center bg-white rounded-full text-base" />
 
-                                            <button onClick={() => { handlesubmit(product[item]) }} className="top-2 right-2 absolute p-0.5 text-gray-900 hover:text-gray-500 flex justify-center items-center bg-white  rounded-full">
+                                            <button onClick={() => { handlesubmit(products[item]) }} className="top-2 right-2 absolute p-0.5 text-gray-900 hover:text-gray-500 flex justify-center items-center bg-white  rounded-full">
                                                 <svg className="fill-stroke" width="15" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                     <path
                                                         d="M6.25 6.875V5.625C6.25 4.63044 6.64509 3.67661 7.34835 2.97335C8.05161 2.27009 9.00544 1.875 10 1.875V1.875C10.9946 1.875 11.9484 2.27009 12.6517 2.97335C13.3549 3.67661 13.75 4.63044 13.75 5.625V6.875M3.125 6.875C2.95924 6.875 2.80027 6.94085 2.68306 7.05806C2.56585 7.17527 2.5 7.33424 2.5 7.5V15.9375C2.5 17.1187 3.50625 18.125 4.6875 18.125H15.3125C16.4937 18.125 17.5 17.1676 17.5 15.9863V7.5C17.5 7.33424 17.4342 7.17527 17.3169 7.05806C17.1997 6.94085 17.0408 6.875 16.875 6.875H3.125Z"
@@ -212,12 +212,12 @@ const Mywishlist = ({ t, product }) => {
                                                 </svg>
                                             </button>
 
-                                            <Link href={`/product/${product[item].slug}`}>
+                                            <Link href={`/product/${products[item].slug}`}>
 
                                                 <span>
 
                                                     {/* {console.log(product[item].slug)} */}
-                                                    <img className="rounded-t-lg w-56 h-44" src={product[item].img} alt="squiggiy" />
+                                                    <img className="rounded-t-lg w-56 h-44" src={products[item].img} alt="squiggiy" />
 
                                                     <div className="flex justify-between mt-4">
                                                         <div>
@@ -243,7 +243,7 @@ const Mywishlist = ({ t, product }) => {
                         </div>
                     </div>
 
-                    {!product && <div className='text-center'>
+                    {!products && <div className='text-center'>
 
                         You have not any product in your wishlist
                     </div>}
